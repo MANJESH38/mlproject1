eff5bc6a3450cb41f87d1b3538db16e19df4600e
Manjesh Kumar <manjeshkumarbgs6@gmail.com>
2026-01-07 22:26:56 +0530

    comment add

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -13,7 +13,9 @@ from src.components.data_transformation import DataTransformationConfig
 from src.components.model_trainer import ModelTrainerConfig
 from src.components.model_trainer import ModelTrainer
 @dataclass
+## isko hmm ek folder me bhi define kr sakte the for starter easy to understand hai
 class DataIngestionConfig:
+    ## Define directly class variables
     train_data_path: str=os.path.join('artifacts',"train.csv")
     test_data_path: str=os.path.join('artifacts',"test.csv")
     raw_data_path: str=os.path.join('artifacts',"data.csv")
@@ -25,16 +27,18 @@ class DataIngestion:
     def initiate_data_ingestion(self):
         logging.info("Entered the data ingestion method or component")
         try:
+            ## Reading code
             df=pd.read_csv(r'notebook\data\stud.csv')
             logging.info('Read the dataset as dataframe')
 
             os.makedirs(os.path.dirname(self.ingestion_config.train_data_path),exist_ok=True)
-
+            ## raw data ko save kiya
             df.to_csv(self.ingestion_config.raw_data_path,index=False,header=True)
 
             logging.info("Train test split initiated")
             train_set,test_set=train_test_split(df,test_size=0.2,random_state=42)
-
+            
+            ## train data ko save kiya
             train_set.to_csv(self.ingestion_config.train_data_path,index=False,header=True)
 
             test_set.to_csv(self.ingestion_config.test_data_path,index=False,header=True)
